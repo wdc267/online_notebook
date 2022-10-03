7799079bfb03cfbfaab868405b23ef18ccb9db78
wangdecai <1599358835@qq.com>
2022-10-03 18:12:28 +0800

    添加删除cell

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@
 let slidebar = document.querySelector('#slidebar');
 let btn = document.querySelector('#slidebar .btn');
 let menu = document.querySelector('.menu');
+let add = document.querySelector('#add');
+let del = document.querySelector('#del');
+let content = document.querySelector('#content');
+let cell = content.children[0];
 let flag = 1;
+// 当前cell编号
+let n = 0;
 // 侧边栏折叠效果
 btn.addEventListener('click', function () {
     if (flag == 1) {
@@ -16,5 +22,19 @@ btn.addEventListener('click', function () {
         menu.style.display = "block";
         btn.innerHTML = "X";
         flag = 1;
+    }
+})
+// 添加cell
+add.addEventListener('click', function () {
+    let i = cell.cloneNode();
+    content.appendChild(i);
+    n++;
+})
+// 删除cell
+del.addEventListener('click', function () {
+    if (n > 0) {
+        let i = content.children[n]
+        content.removeChild(i);
+        n--;
     }
 })

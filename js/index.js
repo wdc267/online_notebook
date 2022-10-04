f37400413af8496897000136ccd0a5c4fb6dee05
wangdecai <1599358835@qq.com>
2022-10-04 17:06:55 +0800

    添加和删除功能的修改

--- js/index.js
+++ js/index.js
@@ -5,36 +5,64 @@ let menu = document.querySelector('.menu');
 let add = document.querySelector('#add');
 let del = document.querySelector('#del');
 let content = document.querySelector('#content');
-let cell = content.children[0];
-let flag = 1;
-// 当前cell编号
-let n = 0;
+let cells = content.children;
+let flag = true;
 // 侧边栏折叠效果
 btn.addEventListener('click', function () {
-    if (flag == 1) {
+    if (flag == true) {
         slidebar.style.width = "10px";
         menu.style.display = "none";
         btn.innerHTML = "√";
-        flag = 0;
+        flag = false;
     }
-    else if (flag == 0) {
+    else if (flag == false) {
         slidebar.style.width = "150px";
         menu.style.display = "block";
         btn.innerHTML = "X";
-        flag = 1;
+        flag = true;
     }
 })
 // 添加cell
-add.addEventListener('click', function () {
-    let i = cell.cloneNode();
-    content.appendChild(i);
-    n++;
-})
+add.addEventListener('click', addCell);
 // 删除cell
-del.addEventListener('click', function () {
-    if (n > 0) {
-        let i = content.children[n]
-        content.removeChild(i);
-        n--;
+del.addEventListener('click',delCell);
+cells[0].addEventListener('focus', addFocus);
+// 在选中的cell后面插入新的cell
+function addCell() {
+    let c = document.createElement('textarea');
+    c.classList.add('textarea');
+    c.addEventListener('focus', addFocus);
+    let i = findFocus();
+    content.insertBefore(c, cells[i + 1]);
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].classList.contains('focusCell'))
+            cells[i].classList.remove('focusCell');
     }
-})
+    c.classList.add('focusCell');
+}
+// 将选中的cell删除
+function delCell() {
+    if (cells.length > 1) {
+        let i = findFocus();
+        content.removeChild(cells[i]);
+        
+    }
+}
+// 添加focusCell类，并标记出来
+function addFocus() {
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].classList.contains('focusCell'))
+            cells[i].classList.remove('focusCell');
+    }
+    this.classList.add('focusCell');
+}
+function findFocus() {
+    let a = 0; //当前有current类的cell的下标
+    for (let i = 0; i <= cells.length; i++) {
+        if (cells[i].classList.contains('focusCell')) {
+            a = i;
+            break;
+        }
+    }
+    return a;
+}
